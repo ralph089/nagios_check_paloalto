d47b6f82ea1c4c7de0d635804fc76cd5348109a6
Ralph Offinger <offinger@hm.edu>
2016-04-29 17:22:43 +0200

    Improved sessinfo command

--- check_pa/sessioninfo.py
+++ check_pa/sessioninfo.py
@@ -16,8 +16,7 @@ def create_check(args):
     """
     return np.Check(
         SessInfo(args.host, args.token),
-        np.ScalarContext('maxsess'),
-        np.ScalarContext('actsess'),
+        np.ScalarContext('session', args.warn, args.crit),
         np.ScalarContext('throughput'),
         SessSummary())
 
@@ -31,9 +30,9 @@ class SessInfo(np.Resource):
 
     def probe(self):
         """
-        Querys the REST-API and create thermal metrics.
+        Querys the REST-API and create session info metrics.
 
-        :return: a disk space metric.
+        :return: a sessinfo metric.
         """
         _log.info('Reading XML from: %s', self.xml_obj.build_request_url())
         soup = self.xml_obj.read()
@@ -42,14 +41,12 @@ class SessInfo(np.Resource):
         actsess = int(Finder.find_item(result, 'num-active'))
         throughput = int(Finder.find_item(result, 'kbps'))
 
-        return [np.Metric('maxsess', maxsess, min=0),
-                np.Metric('actsess', actsess, min=0),
-                np.Metric('throughput', throughput, 'kbps', min=0)]
+        return [np.Metric('session', actsess, min=0, max=maxsess, context='session'),
+                np.Metric('throughput', throughput, 'kbps', min=0, context='throughput')]
 
 
 class SessSummary(np.Summary):
     def ok(self, results):
-        return 'Max possible sessions: ' + str(
-            results['maxsess'].metric) + ' / Active sessions: ' + str(
-            results['actsess'].metric) + ' / Throughput: ' + str(
+        return 'Active sessions: ' + str(
+            results['session'].metric) + ' / Throughput: ' + str(
             results['throughput'].metric)
--- tests/test_sessinfo.py
+++ tests/test_sessinfo.py
@@ -27,6 +27,9 @@ class TestSessionInfo(object):
     @responses.activate
     def test_sessinfo(self):
         f = 'mock_result.xml'
+        self.warn = 20000
+        self.crit = 50000
+
         check = check_pa.sessioninfo.create_check(self)
         obj = check.resources[0]
 
@@ -49,6 +52,67 @@ class TestSessionInfo(object):
 
             assert check.exitcode == 0
             assert check.state == ServiceState(code=0, text='ok')
-            assert check.summary_str == 'Max possible sessions: 262142 ' \
-                                        '/ Active sessions: 4480 ' \
+            assert check.summary_str == 'Active sessions: 4480 ' \
                                         '/ Throughput: 24266kbps'
+
+
+    @responses.activate
+    def test_sessinfo_warning(self):
+        f = 'mock_result.xml'
+        self.warn = 4000
+        self.crit = 50000
+
+        check = check_pa.sessioninfo.create_check(self)
+        obj = check.resources[0]
+
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.GET,
+                     obj.xml_obj.build_request_url(),
+                     body=read_xml(f),
+                     status=200,
+                     content_type='document',
+                     match_querystring=True)
+
+            nummax = 262142
+            numactive = 4480
+            kbps = 24266
+
+            with mock.patch('check_pa.xml_reader.Finder.find_item',
+                            side_effect=[nummax, numactive, kbps]):
+                with pytest.raises(SystemExit):
+                    check.main(verbose=3)
+
+            assert check.exitcode == 1
+            assert check.state == ServiceState(code=1, text='warning')
+            assert check.summary_str == 'session is 4480 (outside range 0:4000)'
+
+
+    @responses.activate
+    def test_sessinfo_warning(self):
+        f = 'mock_result.xml'
+        self.warn = 4000
+        self.crit = 5000
+
+        check = check_pa.sessioninfo.create_check(self)
+        obj = check.resources[0]
+
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.GET,
+                     obj.xml_obj.build_request_url(),
+                     body=read_xml(f),
+                     status=200,
+                     content_type='document',
+                     match_querystring=True)
+
+            nummax = 262142
+            numactive = 5002
+            kbps = 24266
+
+            with mock.patch('check_pa.xml_reader.Finder.find_item',
+                            side_effect=[nummax, numactive, kbps]):
+                with pytest.raises(SystemExit):
+                    check.main(verbose=3)
+
+            assert check.exitcode == 2
+            assert check.state == ServiceState(code=2, text='critical')
+            assert check.summary_str == 'session is 5002 (outside range 0:5000)'
